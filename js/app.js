dc5a2e5a53c8ef97037ee4ae1ab1d41bf550f954
oceane <oceane.gatien.cef@hotmail.fr>
2017-03-10 11:15:37 +0100

    Ajout d'une boucle for+if/else

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("hello")
+for (var i = 0; i <= 100; i++){
+	if (i % 3 == 0){
+		console.log("fizz");
+	}
+	
+	if (i % 5 == 0){
+		console.log("buzz");
+	}
+	
+	if (i % 15 == 0){
+		console.log("fizzbuzz");
+	}
+	
+	else{
+		console.log(i);
+	}
+}
